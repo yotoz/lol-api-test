105bd8fce3f7a4fd4e6eb697aaebddfa0b8bdae7
yotoz <thdrudwls7@gmail.com>
2020-09-08 19:57:57 +0900

    Add query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import './App.css';
-import axios from 'axios';
 
-import { getUserByName } from './Config/config';
+//module
+import {
+	getUserByName,
+	getMatchByAccountId,
+	STATE_CODE,
+	getChampionName,
+	getAllChampions,
+} from './Config/config';
 
 function App() {
+	const [matches, setMatches] = useState([]);
+	const [state, setState] = useState(STATE_CODE[0]);
+	let champions = {};
+
+	const handleGetData = () => {
+		setState(STATE_CODE[1]);
+
+		// getChampionName(62).then((result) => {
+		// 	console.log(result);
+		// });
+
+		getUserByName('Yotoz')
+			.then((user) => {
+				getMatchByAccountId(user.data.accountId)
+					.then((matches) => {
+						setMatches(matches.data.matches);
+						setState(STATE_CODE[999]);
+					})
+					.catch((error) => {
+						console.log(error);
+						setState(STATE_CODE[3]);
+					});
+			})
+			.catch((error) => {
+				console.log(error);
+				setState(STATE_CODE[2]);
+			});
+	};
+
+	useEffect(() => {
+		getAllChampions().then((result) => {
+			console.log(result);
+
+			Object.values(result).forEach((e) => {
+				champions = { ...champions, [e.key]: { ...e } };
+			});
+
+			console.log(champions);
+		});
+	}, []);
+
+	const drawByState = useMemo(() => {
+		switch (state) {
+			case STATE_CODE[0]:
+				return undefined;
+			case STATE_CODE[1]:
+				return <h1>LOADING...</h1>;
+			case STATE_CODE[2]:
+				return <h1>GET USER DATA FAIL</h1>;
+			case STATE_CODE[3]:
+				return <h1>GET MATCH DATA FAIL</h1>;
+			case STATE_CODE[999]:
+				return matches.map((match, idx) => {
+					return (
+						<div key={idx}>
+							game id : {match.gameId}, role : {match.role},
+							champion : {typeof champions['1']}
+						</div>
+					);
+				});
+			default:
+				return undefined;
+		}
+	}, [state]);
+
 	return (
 		<div className="App">
-			<button
-				onClick={() => {
-					getUserByName('Yotoz')
-						.then((result) => {
-							console.log(result);
-						})
-						.catch((error) => {
-							console.log(error);
-						});
-				}}>
-				test
-			</button>
+			<button onClick={handleGetData}>getMatches</button>
+			{drawByState}
 		</div>
 	);
 }
--- src/Config/config.js
+++ src/Config/config.js
@@ -4,11 +4,34 @@ const ApiDefault = {
 	key: 'RGAPI-06367617-241d-4d6b-a14e-01eff285e3eb',
 };
 
-const Url = `/lol/summoner/v4/summoners`;
+const Url = `/lol`;
+const ddragonUrl = `https://ddragon.leagueoflegends.com`;
+
+const getLastestVersion = () => {
+	return new Promise(async (resolve, reject) => {
+		const versionUrl = `${ddragonUrl}/api/versions.json`;
+
+		try {
+			const version = await axios(versionUrl);
+
+			resolve(version.data[0]);
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+export const STATE_CODE = {
+	0: 'unrequest',
+	1: 'loading',
+	2: 'get user data fail',
+	3: 'get match data fail',
+	999: 'success',
+};
 
 export const getUserByName = (name) => {
 	return new Promise(async (resolve, reject) => {
-		const summonerUrl = `${Url}/by-name/${name}?api_key=${ApiDefault.key}`;
+		const summonerUrl = `${Url}/summoner/v4/summoners/by-name/${name}?api_key=${ApiDefault.key}`;
 
 		try {
 			const result = await axios(summonerUrl);
@@ -18,4 +41,52 @@ export const getUserByName = (name) => {
 			reject(error);
 		}
 	});
+};
+
+export const getMatchByAccountId = (accountId) => {
+	return new Promise(async (resolve, reject) => {
+		const matchUrl = `${Url}/match/v4/matchlists/by-account/${accountId}?api_key=${ApiDefault.key}`;
+
+		try {
+			const result = await axios(matchUrl);
+
+			resolve(result);
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+export const getChampionName = (championId) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const lastestVersion = await getLastestVersion();
+
+			const championNameUrl = `${ddragonUrl}/cdn/${lastestVersion}/data/ko_KR/champion.json`;
+			const championName = await axios(championNameUrl);
+
+			Object.values(championName.data.data).forEach((e) => {
+				if (e.key === championId + '') {
+					resolve(e.name);
+				}
+			});
+
+			throw 'champion not found';
+
+			// resolve(championName.data.data);
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+export const getAllChampions = async () => {
+	try {
+		const lastestVersion = await getLastestVersion();
+
+		const championNameUrl = `${ddragonUrl}/cdn/${lastestVersion}/data/ko_KR/champion.json`;
+		const champions = await axios(championNameUrl);
+
+		return champions.data.data;
+	} catch (error) {}
 };
